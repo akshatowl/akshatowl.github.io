baecdbcd7a0ebcce644150ae3c2076085a4ba758
akshatowl <akshatpandeyplus41@gmail.com>
2023-10-30 05:53:30 -0600

    formatting

--- src/Component/Projects.js
+++ src/Component/Projects.js
@@ -44,7 +44,8 @@ function Projects() {
       <p>Seraphim is a project I was a part of during my time at Project MANAS. Seraphim is an <br/>
       autonomous hexacopter based on the PX4 flight stack and is capable of autonomous waypoint<br/> 
       navigation and object detection through a camera module. Seraphim maintains a thrust to <br/>
-      weight ratio of 2:1 while take-off. Seraphim can also carry a payload of upto 7 kg.<br/>
+      weight ratio of 2:1 while take-off. Seraphim can also carry a payload of upto 7 kg.<br/><br/>
+      <a href="https://www.youtube.com/watch?v=CQ0x2UGOluc" style={{ fontSize: '20px', display: 'inline', textDecoration: 'underline' }} target="blank">See it here!</a>
       </p>
     </div>
     <hr/>
@@ -55,7 +56,9 @@ function Projects() {
       </div>
       <p>KOBU is an a system of 3 differential-drive robots that aims to achieve multi-robot<br/>
       coverage planning using the concept of space-filling curves like Peano and Hilbert's <br/>
-      curves. This was implemented using ROS and Gazebo using C++.
+      curves. This was implemented using ROS and Gazebo using C++.<br/><br/>
+      <a href="https://github.com/raghavthakar/kobu" style={{ fontSize: '20px', display: 'inline', textDecoration: 'underline' }} target="blank">See it here!</a>
+      
       </p>
     </div>
     <hr/>
@@ -66,7 +69,8 @@ function Projects() {
       </div>
       <p>I made a simulation to visualize the flight of a quadcopter in Gazebo using ROS. This<br/>
        was a Software-In-Loop testbench I made to send coordinates through a vector in C++<br/>
-       to the PX4 flight stack.
+       to the PX4 flight stack.<br/><br/>
+       <a href="https://github.com/akshatowl/Drone-path-optimizer" style={{ fontSize: '20px', display: 'inline', textDecoration: 'underline' }} target="blank">See it here!</a>
       </p>
     </div>
     <hr/>
@@ -76,7 +80,9 @@ function Projects() {
         <img className="project-image" src="/Images/sloped.png" alt="" />
       </div>
       <p>I worked on implementing slope detection of incident objects through a depth-camera sensor<br/>
-       in python and OpenCV. The script was coverted into publishing the slopes calculated through <br/>ROStopics.
+       in python and OpenCV. The script was coverted into publishing the slopes calculated through <br/>ROStopics.<br/><br/>
+       <a href="https://github.com/akshatowl/VisualSlopeDetection" style={{ fontSize: '20px', display: 'inline', textDecoration: 'underline' }} target="blank">See it here!</a>
+
       </p>
     </div>
     <hr/>
@@ -86,19 +92,24 @@ function Projects() {
         <img className="project-image" src="/Images/ik.png" alt="" />
       </div>
       <p>This was an implementation of iterative inverse kinematics techniques using Jacobian matrices.<br/>
-       I used Newton Raphson root solving method to achieve convergence of a 3 DoF RRR robotic arm. <br/>
+       I used Newton Raphson root solving method to achieve convergence of a 3 DoF RRR robotic arm. <br/><br/>
+       <a href="https://github.com/akshatowl/Iterative-Inverse-Kinematics" style={{ fontSize: '20px', display: 'inline', textDecoration: 'underline' }} target="blank">See it here!</a>
+
       </p>
     </div>
+    <hr/>
     <div>
     <h2>SHELFY</h2>
     <div className="image-container">
-        <img className="project-image" src="/Images/shelfy.jpg" alt="" />
+        <img className="project-image" src="/Images/shelfy.png" alt="" />
       </div>
       <p>My team took part in the Swadeshi Microprocessor Challenge, hosted by the Govt. of India,<br/>
       Ministry of Electronics and Information Technology. We were the national semi-finalists and <br/>
       amoung the top 100 teams out of 3000+ participants. The team was given an indiginous FPGA board<br/>
       to develop a product. We chose to build a smart automated storage and retrieval system for <br/>
-      warehouse automation. 
+      warehouse automation. <br/><br/>
+      <a href="https://drive.google.com/drive/folders/1kyhjSSxPe-VHb6vZ4yJiy1ic7FTwPgd0" style={{ fontSize: '20px', display: 'inline', textDecoration: 'underline' }} target="blank">See it here!</a>
+
       </p>
     </div>
     
